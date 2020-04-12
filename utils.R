abea4a9bae9b6b6f6450e925b79835a89bbbc8f5
Ian Handel <ian.handel@ed.ac.uk>
2020-04-12 14:10:39 +0100

    make flexible

--- utils.R
+++ utils.R
@@ -1,9 +1,8 @@
 # doubling times
 
-doubling <- function(x, n = 7){
+calc_doubling <- function(x, n = 7){
   # n is number of points to count over
-  week <- 7
-  dbl <- week * log(2) / log(x / replace_na(lag(x, week), 0))
+  dbl <- n * log(2) / log(x / replace_na(lag(x, n), 0))
   # zero doubling shouldn't exist
   ifelse(dbl == 0, NA, dbl)
 }

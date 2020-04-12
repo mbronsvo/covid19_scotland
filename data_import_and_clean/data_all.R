543e79143b871d6e6e8c18c639c1bec49b134489
Ian Handel <ian.handel@ed.ac.uk>
2020-04-12 15:01:25 +0100

    start main data script

--- data_import_and_clean/data_all.R
+++ data_import_and_clean/data_all.R
@@ -1,3 +1,13 @@
-# get all the data
+# get all the data -------------------------------------------------------------
+
+library(tidyverse)
+library(lubridate)
 
 source("data_import_and_clean/data_utils.R")
+source("data_import_and_clean/data_scotland.R")
+source("data_import_and_clean/data_ukglobal.R")
+
+data_scot <- cov_scotdata()
+data_uk <- cov_ukdata()
+data_global <- cov_globaldata()
+
